b5dd93cc003af5bff24115e7dfea71e050e86f5b
r041291867 <r041291867@gmail.com>
2019-03-12 10:00:47 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
-Env=$1
 
-/Users/gomez/anaconda3/bin/python3 ./PowerCheck.py ${Env}
+/Users/gomez/anaconda3/bin/python3 ./PowerCheck.py
 
-/Users/gomez/anaconda3/bin/python3 ./BoundaryScan.py ${Env}
+/Users/gomez/anaconda3/bin/python3 ./BoundaryScan.py
 
-/Users/gomez/anaconda3/bin/python3 ./Short.py ${Env}
+/Users/gomez/anaconda3/bin/python3 ./Short.py
 
-/Users/gomez/anaconda3/bin/python3 ./Open.py ${Env}
+/Users/gomez/anaconda3/bin/python3 ./Open.py
 
-/Users/gomez/anaconda3/bin/python3 ./Testjet.py ${Env}
+/Users/gomez/anaconda3/bin/python3 ./Testjet.py
 
-/Users/gomez/anaconda3/bin/python3 ./AnalogPowered.py ${Env}
+/Users/gomez/anaconda3/bin/python3 ./AnalogPowered.py
 
-/Users/gomez/anaconda3/bin/python3 ./Analog.py ${Env}
+/Users/gomez/anaconda3/bin/python3 ./Analog.py
 
+/Users/gomez/anaconda3/bin/python3 ./Digital.py
 
